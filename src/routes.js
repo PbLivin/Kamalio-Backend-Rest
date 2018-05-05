f147c285025c1114aaf881d260290a61087a7613
Dawid Liszka <ddewid@gmail.com>
2018-05-05 16:14:31 +0200

    Revert "renamed auth routes to user routes (#4)"
    This reverts commit 54518b7d8cf0e331f7e1b49b5fc2008ea4fb7951.

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 import { Router } from 'express'
 
 import Authenticate from './middleware/authenticate'
-import userRoutes from './routes/user'
+import authRoutes from './routes/auth'
 import homeRoutes from './routes/home'
 
 const router = Router();
 
-router.use('/user', userRoutes)
+router.use('/auth', authRoutes)
 router.use('/home', Authenticate, homeRoutes)
 
 export default router
