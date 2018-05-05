947fdc4ba68c161f798d31dc6217e41b8d968266
davlis <ddewid@gmail.com>
2018-05-05 16:25:58 +0200

    added update method

--- src/controllers/notification.js
+++ src/controllers/notification.js
@@ -13,3 +13,19 @@ export async function read(req, res) {
     res.json(userNotificationSettings)
 }
 
+export async function update(req, res) {
+    const { user } = res.locals
+    const { UserNotificationSettings } = req.app.get('models')
+    const { allowNotifications } = req.body
+
+    const userNotificationSettings = await UserNotificationSettings.find({
+        where: {
+            userId: user.id
+        }
+    })
+
+    userNotificationSettings.allowNotifications = allowNotifications
+    await userNotificationSettings.save()
+
+    res.json(userNotificationSettings)
+}
