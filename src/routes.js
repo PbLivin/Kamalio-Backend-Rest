2096d76f63d6d28d547ad79c65d53ad6b007bf7c
davlis <ddewid@gmail.com>
2018-05-05 16:38:23 +0200

    removed notification routes

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,12 @@ import { Router } from 'express'
 import Authenticate from './middleware/authenticate'
 import authRoutes from './routes/auth'
 import homeRoutes from './routes/home'
-import notificationRoutes from './routes/notification'
+import userRoutes from './routes/user'
 
 const router = Router();
 
 router.use('/auth', authRoutes)
 router.use('/home', Authenticate, homeRoutes)
-router.use('/notification', Authenticate, notificationRoutes)
+router.use('/user', Authenticate, userRoutes)
 
 export default router
