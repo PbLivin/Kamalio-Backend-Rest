2954e32c3a7772d1ef4a460567d9b8e6052c885f
davlis <ddewid@gmail.com>
2018-06-16 17:02:26 +0200

    fix to endpoint name

--- src/routes/post.js
+++ src/routes/post.js
@@ -12,6 +12,6 @@ router.put('/:id', errorWrap(postController.update))
 router.put('/:id/photo', upload, errorWrap(postController.putPostPhoto))
 router.delete('/:id', errorWrap(postController.remove))
 
-router.post('/:id/vote', errorWrap(postController.vote))
+router.post('/:id/votes', errorWrap(postController.vote))
 
 export default router
