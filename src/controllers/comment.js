11ada7bf65ee4d20babb5709f401b4a70a68fbc7
davlis <ddewid@gmail.com>
2018-06-16 15:52:58 +0200

    added comments interface

---
+++ src/controllers/comment.js
@@ -0,0 +1,21 @@
+import { assertOrThrow } from '../utils'
+
+export async function create(req, res) {
+    res.send('NOT IMPLEMENTED')
+}
+
+export async function list(req, res) {
+    res.send('NOT IMPLEMENTED')
+}
+
+export async function one(req, res) {
+    res.send('NOT IMPLEMENTED')
+}
+
+export async function update(req, res) {
+    res.send('NOT IMPLEMENTED')
+}
+
+export async function remove(req, res) {
+    res.send('NOT IMPLEMENTED')
+}
