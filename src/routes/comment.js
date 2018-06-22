999cfbc9fe72a96d24063b41f4c7649695451b0f
davlis <ddewid@gmail.com>
2018-06-22 23:16:42 +0200

    added comment validation to router

--- src/routes/comment.js
+++ src/routes/comment.js
@@ -1,15 +1,17 @@
 import { Router } from 'express'
 import { errorWrap } from '../utils'
+import validate from '../middleware/validate'
+import schema from '../validators/comment'
 import * as commentController from '../controllers/comment'
 
 const router = Router()
 
-router.post('/', errorWrap(commentController.create))
-router.get('/', errorWrap(commentController.list))
-router.get('/:id', errorWrap(commentController.one))
-router.put('/:id', errorWrap(commentController.update))
-router.delete('/:id', errorWrap(commentController.remove))
+router.post('/', validate(schema.create), errorWrap(commentController.create))
+router.get('/', validate(schema.list), errorWrap(commentController.list))
+router.get('/:id', validate(schema.one), errorWrap(commentController.one))
+router.put('/:id', validate(schema.update), errorWrap(commentController.update))
+router.delete('/:id', validate(schema.remove), errorWrap(commentController.remove))
 
-router.post('/:id/votes', errorWrap(commentController.vote))
+router.post('/:id/votes', validate(schema.vote), errorWrap(commentController.vote))
 
 export default router
